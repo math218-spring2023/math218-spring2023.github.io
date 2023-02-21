055b0bfab7b09503f684549eb295915f9d084c60
Becky Tang <btang96@gmail.com>
2023-02-20 21:57:51 -0500

    Update linear regression slides and add live code 3

--- scratch.R
+++ scratch.R
@@ -1,18 +1,41 @@
-library(class)
-mite_dat
-set.seed(1)
-test_ids <- sample(1:nrow(mite_dat), 20)
+library(vegan)
+cols <- c("#F9C51F", "#FC766AFF", "#9B4A97FF")
+data(mite)
+data(mite.env)
+mite_dat <- mite.env %>%
+  add_column(abundance = mite$LRUG) %>%
+  mutate(present = ifelse(abundance > 0 , T, F))
 
-lm_mod <- lm(abundance ~ .  - Substrate,mite_dat[-test_ids,])
-summary(lm_mod)
-lm_preds <- predict(lm_mod, mite_dat[test_ids,])
-lm_mse <- mean((mite_dat$abundance[test_ids] - lm_preds)^2)
+mite_dat %>%
+  mutate(present = factor(present, levels = c(T, F))) %>%
+  pivot_longer(cols = 1:2, names_to = "variable", values_to = "value") %>%
+  ggplot(., aes(x = present, y = value)) +
+  geom_boxplot() +
+  facet_wrap(~variable, scales = "free") +
+  # plot_theme +
+  ylab("LRUG abundance")
 
-
+mite_dat %>%
+  mutate(present = factor(present, levels = c(T, F))) %>%
+  mutate(Substrate = as.character(Substrate),
+         Shrub = as.character(Shrub)) %>%
+  pivot_longer(cols = 3:5, names_to = "variable", values_to = "value") %>%
+  ggplot(., aes(fill = present, x = value)) +
+  geom_bar(position = "fill") +
+  facet_wrap(~variable, scales = "free_x", nrow = 1,  strip.position = "bottom") +
+  theme(panel.spacing = unit(0, "lines"),
+        panel.border = element_rect(fill = NA),
+        strip.background = element_blank(),
+        axis.title.x = element_blank(),
+        strip.placement = "outside",
+        axis.text.x = element_text(angle = 90))  +
+  scale_fill_manual(values = cols[1:2])
 
 mite_dat2 <- mite_dat %>%
-  mutate(present = ifelse(abundance > 0, 1, 0)) %>%
   dplyr::select(-abundance)
-glm_mod <- glm(present ~ .  - Substrate,mite_dat2, family = "binomial")
-summary(glm_mod)
+mod1 <- glm(present ~ ., data = mite_dat2, family = "binomial")
 
+mod2 <- glm(present ~ . - Substrate, data = mite_dat2, family = "binomial")
+
+summary(mod1)
+summary(mod2)
